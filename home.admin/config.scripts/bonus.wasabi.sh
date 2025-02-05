de84198196862fe01c4adbc2d9cd11777bb23a85
Kevin Ravensberg <kevinravensberg@gmail.com>
2025-02-05 22:34:32 +0100

    New concept bonus.wasabi.sh script
    This script will install and make services for the Wasabi Wallet Coordinator and Backend.

---
+++ home.admin/config.scripts/bonus.wasabi.sh
@@ -0,0 +1,185 @@
+# Wasabi Backend/Coordinator deployment bonus script for RaspiBlitz v1.11.4 (26/01/2025)
+$WasabiVersion="v2.4.0"
+
+PGPsigner="web-flow"
+PGPpubkeyLink="https://github.com/web-flow.gpg"
+PGPpubkeyFingerprint="6FB3872B5D42292F59920797856348328949861E"
+
+# command info
+if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
+  echo "Config script to switch Wasabi Coordinator/Backend on or off"
+  echo "bonus.wasabi.sh menu"
+  echo "bonus.wasabi.sh [install|uninstall]"
+  echo "installs Wasabi Wallet $WasabiVersion"
+  echo "To update to the latest release published on github run:"
+  echo "bonus.wasabi.sh update"
+  echo
+  exit 1
+fi
+
+source /mnt/hdd/raspiblitz.conf
+# get cpu architecture (checked with 'uname -m')
+source /home/admin/raspiblitz.info
+source <(/home/admin/_cache.sh get state)
+
+function WasabiService() {
+  
+  echo "# create the wasabi.service"
+  echo "
+[Unit]
+Description=Wasabi Coordinator daemon
+Requires=bitcoin.service
+After=bitcoin.service
+
+[Service]
+ExecStart=/home/wasabi/dotnet/dotnet run \
+ -c Release --project \"/home/wasabi/WalletWasabi.Coordinator/WalletWasabi.Coordinator.csproj\"
+User=wasabi
+Group=wasabi
+Type=simple
+PIDFile=/run/wasabi/wasabi.pid
+Restart=always
+RestartSec=10
+
+# Hardening measures
+PrivateTmp=true
+ProtectSystem=full
+NoNewPrivileges=true
+PrivateDevices=true
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /etc/systemd/system/wasabi.service
+  sudo systemctl daemon-reload
+}
+
+function BackendService() {
+  
+  echo "# create the wasabibackend.service"
+  echo "
+[Unit]
+Description=Wasabi Backend daemon
+Requires=bitcoin.service
+After=bitcoin.service
+
+[Service]
+ExecStart=/home/wasabi/dotnet/dotnet run \
+ -c Release --project \"/home/wasabi/WalletWasabi.Backend/WalletWasabi.Backend.csproj\"
+User=wasabi
+Group=wasabi
+Type=simple
+PIDFile=/run/wasabi/wasabi.pid
+Restart=always
+RestartSec=10
+
+# Hardening measures
+PrivateTmp=true
+ProtectSystem=full
+NoNewPrivileges=true
+PrivateDevices=true
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /etc/systemd/system/wasabi.service
+  sudo systemctl daemon-reload
+}
+
+########################################
+# INSTALL (just user, code & compile)
+########################################
+
+if [ "$1" = "install" ]; then
+
+  # check if code is already installed
+  isInstalled=$(compgen -u | grep -c wasabi)
+  if [ "${isInstalled}" != "0" ]; then
+    echo "# already installed"
+    exit 0
+  fi
+
+    echo "# create wasabi user"
+    sudo adduser --system --group --home /home/wasabi wasabi
+    cd /home/wasabi || exit 1
+
+    # Clone repository
+    sudo git clone https://github.com/WalletWasabi/WalletWasabi.git
+
+    # Install latest dotnet SDK 8.0
+    sudo wget https://download.visualstudio.microsoft.com/download/pr/bb17a3ab-7122-41bd-96cf-33e35b1d4318/7b09327fdd49b7130cf94838f2979aa6/dotnet-sdk-8.0.405-linux-arm64.tar.gz
+
+    mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-8.0.405-linux-arm64.tar.gz -C $HOME/dotnet
+    export DOTNET_ROOT=$HOME/dotnet
+    export PATH=$PATH:$HOME/dotnet
+
+    # Remove downloaded tar/zip file
+    rm dotnet-sdk-8.0.405-linux-arm64.tar.gz
+
+    # Disable telemetry
+    export DOTNET_CLI_TELEMETRY_OPTOUT=1
+
+    # Build Wasabi Backend
+    cd WalletWasabi/WalletWasabi.Backend/
+    sudo $HOME/dotnet/dotnet build
+
+    # Build Wasabi Coordinator
+    cd ../WalletWasabi.Coordinator
+    sudo $HOME/dotnet/dotnet build
+
+    echo "# make sure wasabi is member of the bitcoin group"
+    sudo /usr/sbin/usermod --append --groups bitcoin wasabi
+
+########################################
+# UNINSTALL (remove from system)
+########################################
+
+if [ "$1" = "uninstall" ]; then
+
+  isActive=$(sudo ls /etc/systemd/system/wasabi.service 2>/dev/null | grep -c 'wasabi.service')
+  if [ "${isActive}" != "0" ]; then
+    echo "# cannot uninstall if still 'on'"
+    exit 1
+  fi
+
+  # clear dotnet cache
+  /home/wasabi/dotnet/dotnet nuget locals all --clear 2>/dev/null
+
+  # remove dotnet
+  sudo rm -rf /usr/share/dotnet 2>/dev/null
+
+  # nuke user
+  sudo userdel -rf wasabi 2>/dev/null
+
+  echo "# uninstall done"
+
+  exit 0
+fi
+########################################
+# UPDATE (pull latest master branch)
+########################################
+echo "# Update Wasabi Wallet"
+sudo -r wasabi git pull -p 
+
+########################################
+# ON (activate & config)
+########################################
+
+if [ "$1" = "1" ] || [ "$1" = "on" ]; then
+  WasabiService
+  BackendService
+  sudo systemctl enable wasabi
+  sudo systemctl start wasabi
+fi
+########################################
+# OFF (deactivate)
+########################################
+
+if [ "$1" = "0" ] || [ "$1" = "off" ]; then
+  # removing service: wasabi
+  sudo systemctl stop wasabi
+  sudo systemctl disable wasabi
+  sudo rm /etc/systemd/system/wasabi.service
+  # removing service: wasabibackend
+  sudo systemctl stop wasabibackend
+  sudo systemctl disable wasabibackend
+  sudo rm /etc/systemd/system/wasabibackend.service
+fi
